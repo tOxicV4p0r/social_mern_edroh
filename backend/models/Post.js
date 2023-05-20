9c507712162314a440284fec341edcda1163bad4
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-05-20 18:26:51 +0700

    first add

---
+++ backend/models/Post.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50
+        },
+        location: String,
+        description: String,
+        picturePath: String,
+        userPicturePath: String,
+        likes: {
+            type: Map,
+            of: Boolean
+        },
+        comments: {
+            type: Array,
+            default: []
+        }
+    },
+    { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
+
